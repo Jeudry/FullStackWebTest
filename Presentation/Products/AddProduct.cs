925c3620030f4e01deb7e5c02a57f01b50147723
Jeudry <jpena@wepsys.net>
2024-06-19 18:52:11 -0400

    updated product record and endpoint

--- Presentation/Products/AddProduct.cs
+++ Presentation/Products/AddProduct.cs
@@ -1,6 +1,17 @@
 namespace Presentation.Products;
 
+/// <summary>
+/// Represents a request to add a product.
+/// </summary>
+/// <param name="Name">Name of the product</param>
+/// <param name="Code">Code of the product</param>
+/// <param name="Description">Description of the product</param>
+/// <param name="Price">Price of the product</param>
+/// <param name="Stock">Stock of the product</param>
 public record AddProductRequest(
     string Name,
-    string Code
+    string Code,
+    string Description,
+    double Price,
+    int Stock
     );
--- WebApi/Controllers/ProductsController.cs
+++ WebApi/Controllers/ProductsController.cs
@@ -7,14 +7,23 @@ using Presentation.Products;
 
 namespace FullStackDevTest.Controllers;
 
+/// <summary>
+/// Represents a controller for products.
+/// </summary>
+/// <param name="sender">MediatR sender</param>
 [ApiController]
 [Route("api/[controller]")]
 public sealed class ProductsController(ISender sender): ControllerBase
 {
+    /// <summary>
+    /// Creates a new product.
+    /// </summary>
+    /// <param name="product">Product to create</param>
+    /// <returns></returns>
     [HttpPost]
     public async Task<ActionResult> CreateProduct(AddProductRequest product)
     {
-        CreateProductCommand command = new CreateProductCommand(product.Name, product.Code);
+        CreateProductCommand command = new CreateProductCommand(product.Name, product.Code, product.Description, product.Price, product.Stock);
         
         await sender.Send(command);
         
